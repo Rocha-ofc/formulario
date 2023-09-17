30b4c1b85721085d958aaf309e467181e76b79fd
Rocha-ofc <jeffersonbelem09@gmail.com>
2023-09-17 16:07:04 -0300

    atualize index.html style.css script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const passwordConfirmation = document.getElementById("password-confirmation");
+
+form.addEventListener("submit",(event) => {
+    event.preventDefault();
+
+    checkInputUsername();
+})
+
+function checkInputUsername(){
+    const usernameValue = username.value;
+
+    if(usernameValue === ""){
+        errorInput(username,"Preencha um usarname!");
+    }
+
+}
+
+function errorInput(input, message){
+    const formItem = input.parentElement;
+    const textMessage = formItem.querySelector("a")
+
+    textMessage.innerText = message;
+
+    formItem.className = "form-content error"
+
+}
